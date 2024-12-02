5f284301385a6c984b1ec2c6b544a14098129156
Yan Lucas <88909517+yanswo@users.noreply.github.com>
2024-12-02 17:42:25 -0300

    Delete Projeto-main (1)/Projeto-main directory

--- Projeto-main (1)/Projeto-main/Projeto-main/Banco48H/src/dao/Teste.java
+++
@@ -1,9 +0,0 @@
-
-package dao;
-
-
-public class Teste {
-     public static void main(String[] args) {
-        ConexaoDB.conectar(); // Isso deve imprimir a mensagem de sucesso ou falha no terminal.
-    }
-}
